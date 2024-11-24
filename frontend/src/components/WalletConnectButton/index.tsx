65a09dd54cb6ca8681f245280242247ab3f8a6d5
HKRK8569 <kayann3021@gmail.com>
2024-11-25 00:47:33 +0900

    ウォレットの接続部分の作成

--- frontend/src/components/WalletConnectButton/index.tsx
+++ frontend/src/components/WalletConnectButton/index.tsx
@@ -1,7 +1,26 @@
+import useWalletStore from "@/stores/walletStore";
+import { useEffect } from "react";
+
 export const WalletConnectButton = () => {
+  const { signer, address, isLoading, connectWallet } = useWalletStore();
+
+  useEffect(() => {
+    connectWallet();
+  }, [connectWallet]);
+  const minifyAddress = (address: string) => {
+    const start = address.substring(0, 5);
+    const end = address.substring(address.length - 4);
+    return `${start}...${end}`;
+  };
+
+  if (signer && address)
+    return <p className="flexh-10">{`👛${minifyAddress(address)}`}</p>;
   return (
-    <button className="flex h-10 w-36 items-center justify-center rounded-full bg-black px-4 text-white">
-      ウォレット接続
+    <button
+      onClick={connectWallet}
+      className="flex h-10 w-40 items-center justify-center rounded-full bg-black px-4 text-white"
+    >
+      {isLoading ? "接続中" : "ウォレット接続"}
     </button>
   );
 };
