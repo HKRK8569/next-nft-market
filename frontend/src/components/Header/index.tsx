63f280c9daa74fa0831de9a56b7e01c325df7780
HKRK8569 <kayann3021@gmail.com>
2024-11-26 02:32:03 +0900

    ヘッダーの修正

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import Link from "next/link";
 import { WalletConnectButton } from "../WalletConnectButton";
+import PAGES from "@/constants/pages";
 
 const Header = () => {
   return (
     <header className="flex h-16 justify-between items-center w-full border-b border-gray-300 px-4">
-      <p>MarketPlace</p>
-      <div></div>
-      <WalletConnectButton />
+      <div className="flex items-center">
+        <p className="mr-8">MarketPlace</p>
+        <div className="flex justify-center">
+          <Link href={PAGES.CREATE}>NFT作成</Link>
+        </div>
+      </div>
+
+      <div className="text-right">
+        <WalletConnectButton />
+      </div>
     </header>
   );
 };
